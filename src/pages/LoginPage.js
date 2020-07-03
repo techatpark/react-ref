98b89870c8e4392633e01a81cd76c013f3cd319b
Sathish Kumar Thiyagarajan <sathishkumar.thiyagarajan@gmail.com>
2020-07-03 18:28:43 +0800

    clean up

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
 
 export default class PlacesPage extends React.Component {
   onClick = async event => {
@@ -10,10 +9,51 @@ export default class PlacesPage extends React.Component {
 
   render() {
     return (
-      <div className="container">
-        <Button variant="primary" onClick={this.onClick}>
-          Login
-        </Button>
+      <div className="container text-center w-25">
+        <form class="form-signin">
+          <img
+            class="mb-4"
+            src="https://v5.getbootstrap.com/docs/5.0/assets/brand/bootstrap-solid.svg"
+            alt=""
+            width="72"
+            height="72"
+          />
+          <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+          <label for="inputEmail" class="sr-only">
+            Email address
+          </label>
+          <input
+            type="email"
+            id="inputEmail"
+            class="form-control"
+            placeholder="Email address"
+            required
+            autoFocus
+          />
+          <label for="inputPassword" class="sr-only">
+            Password
+          </label>
+          <input
+            type="password"
+            id="inputPassword"
+            class="form-control"
+            placeholder="Password"
+            required
+          />
+          <div class="checkbox mb-3">
+            <label>
+              <input type="checkbox" value="remember-me" /> Remember me
+            </label>
+          </div>
+          <button
+            class="btn btn-lg btn-primary btn-block"
+            type="submit"
+            onClick={this.onClick}
+          >
+            Sign in
+          </button>
+          <p class="mt-5 mb-3 text-muted">&copy; 2017-2020</p>
+        </form>
       </div>
     );
   }
