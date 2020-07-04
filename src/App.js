630d109d8098809feba77a507fbfee43dfd2a9d8
Sathish Kumar Thiyagarajan <sathishkumar.thiyagarajan@gmail.com>
2020-07-04 20:39:03 +0800

    bootstarp now

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import './stylesheet/styles.css';
-import { Home } from './components/Home';
-import { Addemployee } from './components/Addemployee';
-import { Editemployee } from './components/Editemployee';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import { Home } from "./components/Home";
+import { Addemployee } from "./components/Addemployee";
+import { Editemployee } from "./components/Editemployee";
 
-
-import { GlobalProvider } from './context/GlobalState';
+import { GlobalProvider } from "./context/GlobalState";
 
 function App() {
   return (
