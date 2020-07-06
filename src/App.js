5007148142710d12b33cc3ff5920b634b10fcef3
apt-get2Update <thirumurugan3131@gmail.com>
2020-07-06 22:13:22 +0530

    wrapper added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import { Home } from "./components/Home";
-import { AddEditEmployee, Employeelist } from "./module/employee"
-import { Departmentlist, AddEdit } from "./module/department"
+import { AddEditEmployee, Employeelist } from "./module/employee";
+import { Departmentlist, AddEdit } from "./module/department";
 
 import { GlobalProvider } from "./context/GlobalState";
+import { Wrapper } from "./components/Wrapper";
 
 function App() {
   return (
     <GlobalProvider>
-      <Switch>
-        <Route path="/" component={Home} exact />
-        <Route path="/employee" component={Employeelist} exact />
-        <Route path="/employee/add" component={AddEditEmployee} exact />
-        <Route path="/employee/edit/:id" component={AddEditEmployee} exact />
-        <Route path="/department" component={Departmentlist} exact />
-        <Route path="/department/add" component={AddEdit} exact />
-      </Switch>
+      <Wrapper>
+        <Switch>
+          <Route path="/" component={Home} exact />
+          <Route path="/employee" component={Employeelist} exact />
+          <Route path="/employee/add" component={AddEditEmployee} exact />
+          <Route path="/employee/edit/:id" component={AddEditEmployee} exact />
+          <Route path="/department" component={Departmentlist} exact />
+          <Route path="/department/add" component={AddEdit} exact />
+        </Switch>
+      </Wrapper>
     </GlobalProvider>
   );
 }
---
+++ src/components/Wrapper.js
@@ -0,0 +1,57 @@
+import React, { Fragment } from "react";
+import { Link } from "react-router-dom";
+
+export const Wrapper = ({children}) => {
+  return (
+    <Fragment>
+      <nav className="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+        <div className="container-fluid">
+          <a href="/" className="navbar-brand d-flex align-items-center">
+            React CRUD
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarsExampleDefault"
+            aria-controls="navbarsExampleDefault"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarCollapse">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+              <Link to="/employee" className="nav-link active">Employees</Link>
+              </li>
+              <li className="nav-item">
+              <Link to="/department" className="nav-link active">Departments</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <ul className="navbar-nav ml-sm-auto">
+          <li className="nav-item active">
+            <a
+              className="nav-link active"
+              href="https://github.com/twbs"
+              rel="noopener noreferrer"
+              target="_blank"
+              aria-label="GitHub"
+            >
+              <i className="fas fa-power-off"></i>
+            </a>
+          </li>
+        </ul>
+      </nav>
+
+      <div className="container-fluid mt-3 vh-100 pb-5">
+        <section className="row mt-5 p-2">
+          {children}
+        </section>
+      </div>
+    </Fragment>
+  );
+};
